6189d312d4b5f421212cd613eddd5c68ea75f1bc
Thiago <thiagodeboliveira@outlook.com>
2025-01-07 11:20:33 -0500

    adding set method to linked list

--- main/Main.java
+++ main/Main.java
@@ -2,11 +2,15 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList myLinkedList = new LinkedList(4);
+        LinkedList myLinkedList = new LinkedList(0);
 
-        myLinkedList.getHead();
-        myLinkedList.getTail();
-        myLinkedList.getLength();
+        myLinkedList.append(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+
+        System.out.println(myLinkedList.get(2).value + "\n");
         myLinkedList.printList();
+
+
     }
 }
